1a2cd2540dd15c9c53e4a43f2029d7d473fa78c5
Devadharshinisubiksha <52270298+Devadharshinisubiksha@users.noreply.github.com>
2019-07-11 20:35:45 +0530

    Create diary.py

---
+++ diary.py
@@ -0,0 +1,8 @@
+ch1=int(input())
+cho11=[int(o) for o in input().split(" ")]
+che12=0
+for p1 in range(ch1):
+      for g1 in range(p1):
+           if(cho11[g1]<cho11[p1]):
+                che12+=cho11[g]
+print(che12)
