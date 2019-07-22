8ef3f08f3636238c138b99631c3e2bb8f7e16ae4
Devadharshinisubiksha <52270298+Devadharshinisubiksha@users.noreply.github.com>
2019-07-22 11:48:40 +0530

    Create kindergardenpro.py

---
+++ kindergardenpro.py
@@ -0,0 +1,9 @@
+abiksha1=int(input())
+ba112=0
+l=[]
+for abiksha1 in range(1,abiksha1+1):
+  l.append(abiksha1)
+for abiksha1 in range(len(l)):
+  for ab1 in range(abiksha1+1,len(l)):
+    ba112+=1
+print(ba112)
