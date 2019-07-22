b40b3f9b0d27b235dab34a21bf6f4737fae0960c
Devadharshinisubiksha <52270298+Devadharshinisubiksha@users.noreply.github.com>
2019-07-22 10:57:42 +0530

    Update printinwords.py

--- printinwords.py
+++ printinwords.py
@@ -1,4 +1,4 @@
 character=["One","Two","Three","Four","Five","six","Seven","Eight","Nine","Ten"]
-subi=int(input())
-if (subi>=1 and subi<=10):
-   print(character[subi-1])
+subiksha=int(input())
+if (subiksha>=1 and subiksha<=10):
+   print(character[subiksha-1])
