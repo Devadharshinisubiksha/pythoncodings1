abd3b4226199afeb8cf50348d13cccd38fdda247
Devadharshinisubiksha <52270298+Devadharshinisubiksha@users.noreply.github.com>
2019-08-05 14:36:48 +0530

    Create proo27.py

---
+++ proo27.py
@@ -0,0 +1,17 @@
+A16,Bbt1=map(int,input().split())
+Cct1=list(map(int,input().split()))
+prt1=list(map(int,input().split()))
+qrt1=[]
+art1=0
+for i in range(A16):
+    xt=prt1[i]/Cct1[i]
+    qrt1.append(xt)
+while Bbt1>=0 and len(qrt1)>0:
+    mindex=qrt1.index(max(qrt1))
+    if Bbt1>=Cct1[mindex]:
+        art=art1+prt1[mindex]
+        Bbt=Bbt1-Cct1[mindex]
+    Cct1.pop(mindex)
+    prt1.pop(mindex)
+    qrt1.pop(mindex)
+print(art1)
